7be011b8c833ff2e8a5e275a5eb97c038a38bb12
mounir labaied <mounir.labaied@ms-exchanger.com>
2018-03-25 16:40:08 +0200

    last trade with love

--- HL.js
+++ HL.js
@@ -6,20 +6,25 @@ var method = {};
 
 method.init = function() {
     this.name = 'Scalper';
-
+    this.addTulipIndicator('ps', 'psar', {optInAcceleration:0.25,
+        optInMaximum:0.50
+    });
+    
     this.candle_queue = [];
     this.is_buyin = false;
     this.price_buyin = 0;
 }
 
 var barscount = 0;
-var lastlow = 0;
-method.update = function(candle) {
+var DarvasHigh = 0;
+var DarvasLow = 0;
 
-    if(candle.low < lastlow){lastlow = candle.low;}
-    if(this.candle_queue.length>Period){
-        this.candle_queue.pop();
-    }
+method.update = function(candle) {
+    this.psar = this.tulipIndicators.ps.result.result;
+
+    if(candle.low < DarvasLow){DarvasLow = candle.low;}
+    if(candle.high < DarvasHigh){DarvasHigh = candle.low;}
+
     this.candle_queue.push(candle);
     barscount++;
     if(this.candle_queue.length>0){
@@ -32,6 +37,7 @@ var distance = 3;
 var Period = 14;
 var lastcolor = 0;
 var Min = [];
+var MovingTR = [];
 IsReversalUp = function(min,candle){
 
     var c1 = this.candle_queue[this.candle_queue.length -2];
@@ -61,27 +67,38 @@ method.check = function(candle) {
             if(bar.close >= runninMax)
             {
                 var runninMax  = bar.close;
-                log.debug(runninMax);
 
             }
         }
 
-        log.debug(runninMax);
-        var LowerLow = Min[Min.length -2] > Min[Min.length -1];
-        log.debug("Lower Low :", LowerLow);
+        var LowerLow = Min[Min.length -1] > Min[0];
         var CandeLow = this.candle.close < runningMin && (this.candle.close - runningMin) / 100;
         MoveCycle.push((this.candle.close - runningMin) / 100);
-        var Downslow = MoveCycle[MoveCycle.length -2] > MoveCycle[MoveCycle.length -1];
-        if(CandeLow && Downslow)
+        var Downslow = MoveCycle[MoveCycle.length -1] > MoveCycle[0];
+        
+
+        var c1 = this.candle_queue[this.candle_queue.length -2];
+        var TrueRange = Math.max(runninMax,c1.close) - Math.min(runningMin,c1.close);
+        var valid = TrueRange / (candle.close - c1.close);
+        var Range = 100 * ((valid - runningMin) / (runninMax - runningMin));
+        MovingTR.push(valid);
+        var MovingSlower = MovingTR[MovingTR.length -2] > valid;
+
+
+        if(CandeLow  &&! MovingSlower && valid > 0 &&! this.is_buyin && this.candle.close > this.psar)
         {
-            log.debug("Lower move : ",(this.candle.close - runningMin) / 100)
             this.price_buyin = candle.close;
+            log.debug("valid : ",valid);
+            this.candle_queue.length = 0;
+            runningMin = 0;
+            runninMax = 0;
+            Downslow.length = 0;
+
             this.is_buyin = true;
             return this.advice("long");
         }
-        else if (candle.close >= runninMax &! Downslow)
+        else if (candle.close >= runninMax && this.is_buyin  )
         {
-            
                 this.is_buyin = false;
                 return this.advice("short");
         }
