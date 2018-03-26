831c3a96e5c7f96e780aba66c3a67f3e930f0a19
mounir labaied <mounir.labaied@ms-exchanger.com>
2018-03-26 16:06:40 +0200

    update

--- HL.js
+++ HL.js
@@ -1,6 +1,6 @@
 var _ = require('lodash');
 var log = require('../core/log.js');
-
+var math = require('mathjs')
 
 var method = {};
 
@@ -34,21 +34,24 @@ method.update = function(candle) {
 }
 var percent = 35;
 var distance = 3;
-var Period = 14;
+var Period = 25;
 var lastcolor = 0;
 var Min = [];
 var MovingTR = [];
+var NoTradedSince = 0; 
 IsReversalUp = function(min,candle){
 
     var c1 = this.candle_queue[this.candle_queue.length -2];
     return (candle.low < min && candle.close > c1.close);
-
 }
 
 var MoveCycle = [];
+var LowTopDif = [];
 method.check = function(candle) {
-    if (this.candle_queue.length >= 15)
+    if (this.candle_queue.length >= Period)
     {
+
+       //Get Min Max
         runningMin = 99999999;
         runninMax = 0;
         for (var barsBack = Math.min(this.candle_queue.length, Period - 1); barsBack > 0; barsBack--)
@@ -70,6 +73,8 @@ method.check = function(candle) {
 
             }
         }
+       //Get Min Max EOF
+
 
         var LowerLow = Min[Min.length -1] > Min[0];
         var CandeLow = this.candle.close < runningMin && (this.candle.close - runningMin) / 100;
@@ -83,25 +88,52 @@ method.check = function(candle) {
         var Range = 100 * ((valid - runningMin) / (runninMax - runningMin));
         MovingTR.push(valid);
         var MovingSlower = MovingTR[MovingTR.length -2] > valid;
+        var RangeControl = valid != Infinity;
+
+        LowTopDif.push((runningMin - runninMax) / 100);
+        var BoxExpanding = LowTopDif[LowTopDif.length -2] < LowTopDif[LowTopDif.length -1];
 
 
-        if(CandeLow  &&! MovingSlower && valid > 0 &&! this.is_buyin && this.candle.close > this.psar)
+
+        // log.debug('Min: ',runningMin);
+        // log.debug('Max: ',runninMax);
+        if(CandeLow   && valid > 0 &&! this.is_buyin)
         {
             this.price_buyin = candle.close;
-            log.debug("valid : ",valid);
             this.candle_queue.length = 0;
             runningMin = 0;
             runninMax = 0;
+            Min.length = 0;
             Downslow.length = 0;
-
+            MovingTR.length = 0;
+            MovingSlower.length = 0;
             this.is_buyin = true;
             return this.advice("long");
         }
         else if (candle.close >= runninMax && this.is_buyin  )
         {
-                this.is_buyin = false;
-                return this.advice("short");
+            this.candle_queue.length = 0;
+            runningMin = 0;
+            runninMax = 0;
+            Min.length = 0;
+            MovingTR.length = 0;
+            Downslow.length = 0;
+            MovingSlower.length = 0;
+            this.is_buyin = false;
+            return this.advice("short");
         }
+        if(NoTradedSince > 2 &&! this.is_buyin)
+        {
+            this.candle_queue.length = 0;
+            runningMin = 0;
+            Min.length = 0;
+            runninMax = 0;
+            Downslow.length = 0;
+            MovingTR.length = 0;
+            MovingSlower.length = 0;
+            NoTradedSince = 0;
+        }
+        NoTradedSince++;
     
     }
 }
